c94b39d9506876378b599407173a5ef8cfea150d
phil-or-4 <phillipsopt@gmail.com>
2018-12-31 14:31:34 -0600

    started working on position function

--- ph-engine.py
+++ ph-engine.py
@@ -1,5 +1,8 @@
 import chess
 
+
+
+board = chess.Board()
 author = 'Phillip Sopt'
 engineName = 'PhEngine'
 
@@ -8,13 +11,16 @@ def main():
         uciIn = input()
         if uciIn == 'uci':
             iUci()
-        elif 'setoption' in uciIn[:9]:
-            iSetOption(uciIn[10:])      # 10 to skip the whitespace
+        elif uciIn.startswith('setoption'):
+            iSetOption(uciIn[10:])      # 10 to skip the 'setoption'
         elif uciIn == 'isready':
             iIsReady()
         elif uciIn == 'ucinewgame':
             iNewGame()
-        elif 'position' in uciIn[:8]
+        elif uciIn.startswith('position'):
+            iPosition(uciIn[9:])        # 9 to skip the 'position'
+        
+
     
 
 
@@ -27,9 +33,25 @@ def iUci():
 
 def iSetOption(inStr):
     #set the option
+    pass
 
 def iIsReady():
     print('readyok')
+
+def iNewGame():
+    pass
+
+def iPosition(inStr):
+    if inStr.startswith('startpos'):
+        inStr = inStr.partition(' ')[3]     # partition string 
+        board.set_board_fen(chess.STARTING_BOARD_FEN)
+        #do a while loop till there's no more input
+        if
+
+    elif inStr.startswith('fen'):
+        board.set_board_fen(inStr[4:])  # use the fen string at the end to build a new board
+    
+
 
             
 
